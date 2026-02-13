4d745e57bbcaef698d2c315ff53c062abbbb9533
fauzanashshidiq <muhzeen07@gmail.com>
2026-02-13 16:03:50 +0700

    Add protect endpoint for post articles

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Get, Post, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
-import { Article } from './article.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Query('userId') userId: number, @Body() body: Partial<Article>) {
-    return this.articlesService.create(Number(userId), body);
+  create(@Req() req, @Body() body: any) {
+    const userId = req.user.userId;
+    return this.articlesService.create(userId, body);
   }
 
   @Get()
